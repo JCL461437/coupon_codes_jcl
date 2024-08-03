5555f007a2585420fb7cff59d34c97878022c262
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-03 14:42:11 -0600

    Add the new and create actions in the coupons controller. Need sad path implementation still

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -8,4 +8,19 @@ class CouponsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @coupon = @merchant.coupons.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = @merchant.coupons.find(params[:id])
+
+    Coupon.create!(name: params[:name],
+    unique_code: params[:unique_code],
+    dollar_off: params[:dollar_off],
+    percent_off: params[:percent_off],
+    merchant: @merchant)
+    redirect_to merchant_coupons_path(@merchant)
+  end
 end
