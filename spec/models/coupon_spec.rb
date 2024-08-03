570d02b09c02ac9db9d5f222ac5e7877bb02a16f
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-03 11:05:15 -0600

    Add before each block for coupon_spec for uniqueness error and created subject in validations

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 RSpec.describe Coupon, type: :model do
+  before (:each) do 
+    merchant = Merchant.create(name: "Craig Jones")
+    coupon1 = Coupon.create(name: "Five Dollars Off!", unique_code: "A238HFSD82", dollar_off: 500,  percent_off: 0, merchant: merchant )
+    coupon2 = Coupon.create(name: "Five Percent Off!", unique_code: "GL12FG3FJ6", dollar_off: 0, percent_off: 0.05, merchant: merchant )
+  end
+
   describe "validations" do
+    subject { Coupon.create(name: "Test Coupon", unique_code: "B1290SA8S2", percent_off: 0) }
     it { should validate_presence_of :name }
     
     it { should validate_numericality_of :dollar_off }
