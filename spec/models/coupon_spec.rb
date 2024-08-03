bcb6ae596b112196fbcc1f74f3515d31ef69a997
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-02 20:01:23 -0600

    Add validation tests and association tests to the coupon_spec.rb file. Error raised for line 12 with syntax error, unexpected end

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
-
-describe Merchant do
+RSpec.describe Coupon, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
+    
+    it { should validate_numericality_of :dollar_off }
+    it { should validate_numericality_of :percent_off }
+
+    it { should validate_uniqueness_of :unique_code }
+
     it { should validate_presence_of :percent_off }
     it { should validate_presence_of :dollar_off }
-    it { should validate_presence_of :unique_code}
-
-    it {should validates_numericality_of :dollar_off}
-    it {should validates_numericality_of :dollar_off}
-
-    it {should validates_uniqueness_of :unique_code}
+    it { should validate_presence_of :unique_code }
   end
+
   describe "relationships" do
     it { should have_many :invoices }
     it { should belong_to :merchant }
   end
-
+  
 end
