1c2dd0c351cba506c34434021fe787ce35215287
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-03 16:18:40 -0600

    Add flow control in create checking for dollar_off and percent_off presence and absence for us 2 functionality

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -15,12 +15,17 @@ class CouponsController < ApplicationController
   def create
     @merchant = Merchant.find(params[:merchant_id])
     @coupon = @merchant.coupons.find(params[:id])
+    if params[:dollar_off].blank? && params[:percent_off].present? || params[:dollar_off].present? && params[:percent_off].empty? # cheks to see if either the dollar is empty and percent is full or percent is pull and dollar is empty. If either is true good to go. 
+      Coupon.create!(name: params[:name],
+      unique_code: params[:unique_code],
+      dollar_off: params[:dollar_off],
+      percent_off: params[:percent_off],
+      merchant: @merchant)
+      redirect_to merchant_coupons_path(@merchant)
+    elsif params[:dollar_off].present? && params[:percent_off].present?
+      
+    else 
 
-    Coupon.create!(name: params[:name],
-    unique_code: params[:unique_code],
-    dollar_off: params[:dollar_off],
-    percent_off: params[:percent_off],
-    merchant: @merchant)
-    redirect_to merchant_coupons_path(@merchant)
+    end
   end
 end
