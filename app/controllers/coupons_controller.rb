9090b3fbd67d28f789f20748877ba94ab1e7fe8c
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-04 09:28:10 -0600

    Add more flow control for create action to handle issue of empty dollar off or percent off column not fulfilling validation by setting it to 0 if it is empty

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -18,16 +18,26 @@ class CouponsController < ApplicationController
 
     if params[:dollar_off].present? && params[:percent_off].present? # If user has filled information for both the dollar and percet columns it will not create a coupon and prompt them to do it again
       flash.notice = "You may only enter a value for either dollar off or percent off."
-    elsif unique_code?(params[:unique_code]) #pass param of unique code into method to query db and make sure it is unique
-      flash.notice = "That code is already assigned to a coupon. Enter a different code."
+    #elsif #pass param of unique code into method to query db and make sure it is unique
+      #flash.notice = "That code is already assigned to a coupon. Enter a different code."
     else 
-      Coupon.create!(name: params[:name],
-      unique_code: params[:unique_code],
-      dollar_off: params[:dollar_off],
-      percent_off: params[:percent_off],
-      merchant: @merchant)
-      flash.notice = 'Coupon has been created!'
-      redirect_to merchant_coupons_path(@merchant)
+      if params[:dollar_off].empty?
+        Coupon.create!(name: params[:name],
+        unique_code: params[:unique_code],
+        dollar_off: 0,
+        percent_off: params[:percent_off],
+        merchant: @merchant)
+        flash.notice = 'Coupon has been created!'
+        redirect_to merchant_coupons_path(@merchant)
+      elsif params[:percent_off].empty?
+        Coupon.create!(name: params[:name],
+        unique_code: params[:unique_code],
+        dollar_off: params[:dollar_off],
+        percent_off: 0,
+        merchant: @merchant)
+        flash.notice = 'Coupon has been created!'
+        redirect_to merchant_coupons_path(@merchant)
+      end
     end
   end
 end
