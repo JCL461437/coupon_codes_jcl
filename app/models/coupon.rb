ea89a1313b97f3304f5a72104710b0bdd8133460
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-04 11:00:30 -0600

    Add enum for coupon for status column and validations for status

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,8 +1,9 @@
 class Coupon < ApplicationRecord
   validates_presence_of :name,
-                        :percent_off#, { unless: :dollar_off },
-                        :dollar_off#, { unless: :percent_off },
-                        :unique_code 
+                        :percent_off,#, { unless: :dollar_off },
+                        :dollar_off,#, { unless: :percent_off },
+                        :unique_code,
+                        :status
   validates_numericality_of :percent_off
   validates_numericality_of :dollar_off
   validates_uniqueness_of :unique_code
@@ -10,4 +11,8 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   has_many :invoices #optional: true
 
+  enum status: {
+    inactive: 0,
+    active: 1,
+  }
 end
